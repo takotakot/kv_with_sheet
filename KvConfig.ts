6154a1b5b95d4a71e4047bbb5fe5f0b6442c5488
takotakot <takotakot@users.noreply.github.com>
2023-04-22 13:20:03 +0900

    feat: Update kvConfig

--- KvConfig.ts
+++ KvConfig.ts
@@ -9,29 +9,93 @@ class KvConfig {
     this.readFromSheet();
   }
 
-  private readFromSheet() {
-    const dataRange = this.sheet.getDataRange();
-    const values = dataRange.getValues();
-    const headers = values.shift();
-    const sheetIdIndex = headers.indexOf('sheet_id');
-    const sheetNameIndex = headers.indexOf('sheet_name');
-    const colIdIndex = headers.indexOf('col_id');
-    const colNameIndex = headers.indexOf('col_name');
-    for (const row of values) {
-      if (row[sheetIdIndex] && row[sheetNameIndex]) {
-        this.sheetNames.push({
-          sheet_id: row[sheetIdIndex],
-          sheet_name: row[sheetNameIndex],
-        });
-      }
-      if (row[sheetIdIndex] && row[colIdIndex] && row[colNameIndex]) {
-        this.sheetColumnNames.push({
-          sheet_id: row[sheetIdIndex],
-          col_id: row[colIdIndex],
-          col_name: row[colNameIndex],
-        });
+  private readFromSheet(): void {
+    const blocks = this.splitIntoBlocks();
+    for (const block of blocks) {
+      if (this.isSheetNamesBlock(block)) {
+        this.processSheetNamesBlock(block);
+      } else if (this.isSheetColumnNamesBlock(block)) {
+        this.processSheetColumnNamesBlock(block);
       }
     }
+  }
+  
+  private processSheetNamesBlock(rows: string[][]): void {
+    const headerRow = rows[0];
+    const sheetNames = [];
+    for (let i = 1; i < rows.length; i++) {
+      const row = rows[i];
+      const sheetName = row[headerRow.indexOf("sheet_name")];
+      const sheetId = row[headerRow.indexOf("sheet_id")];
+      sheetNames.push({ sheet_id: sheetId, sheet_name: sheetName });
+    }
+    this.sheetNames = sheetNames;
+  }
+  
+  private processSheetColumnNamesBlock(rows: string[][]): void {
+    const headerRow = rows[0];
+    const sheetColumns: SheetColumnNames = [];
+  
+    for (let i = 2; i < rows.length; i++) {
+      const row = rows[i];
+      const sheetId = row[headerRow.indexOf("sheet_id")];
+      const colId = row[headerRow.indexOf("col_id")];
+      const colName = row[headerRow.indexOf("col_name")];
+      sheetColumns.push({ sheet_id: sheetId, col_id: colId, col_name: colName });
+    }
+  
+    this.sheetColumnNames = sheetColumns;
+  }
+
+  private splitIntoBlocks(): string[][][] {
+    const blocks = [];
+    const numRows = this.sheet.getLastRow();
+    const numCols = this.sheet.getLastColumn();
+    const range = this.sheet.getRange(1, 1, numRows, numCols);
+    const values = range.getValues();
+    const transposed = values[0].map((_, c) => values.map((r) => r[c]));
+    let currentBlock = [];
+    for (const column of transposed) {
+      if (column.some((cellValue) => cellValue)) {
+        // Column has at least one non-empty cell
+        currentBlock.push(column);
+      } else if (currentBlock.length > 0) {
+        // End of block
+        blocks.push(currentBlock);
+        currentBlock = [];
+      }
+    }
+    if (currentBlock.length > 0) {
+      // Add last block
+      blocks.push(currentBlock);
+    }
+    return blocks;
+  }
+
+  private isSheetNamesBlock(block: string[][]): boolean {
+    const expectedColumns = ["sheet_id", "sheet_name"];
+    const headerRow = block[0];
+  
+    // 期待するカラム名がすべて含まれているか確認する
+    const includesAllColumns = expectedColumns.every((col) =>
+      headerRow.includes(col)
+    );
+  
+    // 期待するカラム名がすべて含まれている場合は true を返す
+    return includesAllColumns;
+  }
+
+  private isSheetColumnNamesBlock(block: string[][]): boolean {
+    const expectedColumns = ["sheet_id", "col_id", "col_name"];
+    const headerRow = block[0];
+  
+    // 期待するカラム名がすべて含まれているか確認する
+    const includesAllColumns = expectedColumns.every((col) =>
+      headerRow.includes(col)
+    );
+  
+    // 期待するカラム名がすべて含まれている場合は true を返す
+    return includesAllColumns;
   }
 
   getSheetNames(): SheetNames {
